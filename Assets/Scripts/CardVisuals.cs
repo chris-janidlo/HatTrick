cc2089ff42bffcd852fe8a6d1c87a5e29461e151
crassSandwich <christopherjanidlo@gmail.com>
2017-09-27 16:58:01 -0400

    Card Dragging

--- Assets/Scripts/CardVisuals.cs
+++ Assets/Scripts/CardVisuals.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class CardVisuals : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+//class that handles rendered cards and their interaction with input
+public class CardVisuals : MonoBehaviour, 
+	IPointerEnterHandler, IPointerExitHandler,
+	IBeginDragHandler, IDragHandler, IEndDragHandler {
 
 	public Vector2 HilightScale; //size when mousing over in hand
 	public Vector2 LolightScale; //size when not mousing over
 
 	private Text placeholder;
+	private Vector2 posCache;
 
 	void Awake () {
 		placeholder = GetComponentInChildren<Text>();
@@ -27,4 +31,15 @@ public class CardVisuals : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 		transform.localScale = LolightScale;
 	}
 
+	public void OnBeginDrag (PointerEventData eventData) {
+		posCache = transform.position;
+	}
+
+	public void OnDrag (PointerEventData eventData) {
+		transform.position = eventData.position;
+	}
+
+	public void OnEndDrag (PointerEventData eventData) {
+		transform.position = posCache;
+	}
 }
