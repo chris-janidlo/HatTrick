4dd3cdeb012ebb1cf28a468686c787899d47e36d
crassSandwich <christopherjanidlo@gmail.com>
2017-10-19 16:03:03 -0400

    better Suit

--- Assets/Scripts/Suit.cs
+++ Assets/Scripts/Suit.cs
@@ -3,81 +3,51 @@ using UnityEngine;
 
 namespace HatTrick {
 
-public enum SuitColor {
-	yellow,
-	blue,
-	green,
-	orange,
-	white,
-	red,
-	black
+public enum SuitName {
+	C, D, E, F, G, A, B
 }
 
-//Suit is almost like an enum: there are only 7 possible values accessible from the outside, which are the properties at the end of the definition
-//it isn't an enum, however, in that each value is a struct with three publically accessible values
-[System.Serializable]
-public class Suit {
-	public SuitColor root; //color of the chord where this note is the root
-	public SuitColor third; //color of the chord where this note is the third
-	public SuitColor fifth; //color of the chord where this note is the fifth
+public enum SuitColor {
+	yellow, blue, green, orange, white, red, black
+}
 
-	private Suit (SuitColor _root, SuitColor _third, SuitColor _fifth) {
-		root = _root;
-		third = _third;
-		fifth = _fifth;
+public class Suit {
+
+	public SuitName Label { get; private set; }
+
+	/// color of the chord where this note is the root
+	public SuitColor Root {
+		get { return (SuitColor) ((int) Label); }
+	}
+	/// color of the chord where this note is the third
+	public SuitColor Third {
+		get { return (SuitColor) ((((int) Label) - 2) % 7); }
+	}
+	/// color of the chord where this note is the fifth
+	public SuitColor Fifth {
+		get { return (SuitColor) ((((int) Label) - 4) % 7); }
 	}
 
-	public static Suit IntToSuit (int noteNumber) {
-		switch (noteNumber) {
-			case 1:
-				return Suit.C;
-			case 2:
-				return Suit.D;
-			case 3:
-				return Suit.E;
-			case 4:
-				return Suit.F;
-			case 5:
-				return Suit.G;
-			case 6:
-				return Suit.A;
-			case 7:
-				return Suit.B;
-			default:
-				throw new System.ArgumentException();
-		}
+	private Suit (SuitName label) {
+		Label = label;
 	}
 
 	public static Suit RandomSuit () {
-		return IntToSuit(Random.Range(1, 8));
+		return new Suit((SuitName) Random.Range(0, 7));
 	}
-
-	public static Suit C { get {
-		return new Suit(SuitColor.yellow, SuitColor.red, SuitColor.orange);
-	} }
-	public static Suit D { get {
-		return new Suit(SuitColor.blue, SuitColor.black, SuitColor.white);
-	} }
-	public static Suit E { get {
-		return new Suit(SuitColor.green, SuitColor.yellow, SuitColor.red);
-	} }
-	public static Suit F { get {
-		return new Suit(SuitColor.orange, SuitColor.blue, SuitColor.black);
-	} }
-	public static Suit G { get {
-		return new Suit(SuitColor.white, SuitColor.green, SuitColor.yellow);
-	} }
-	public static Suit A { get {
-		return new Suit(SuitColor.red, SuitColor.orange, SuitColor.blue);
-	} }
-	public static Suit B { get {
-		return new Suit(SuitColor.black, SuitColor.white, SuitColor.green);
-	} }
 
 	public override string ToString () {
-		return root.ToString() + ", " + third.ToString() + ", " + fifth.ToString();
+		return Label.ToString();
 	}
-	
+
+	/// Returns whether this shares a color with other.
+	public bool HarmonizesWith (Suit other) {
+		return  Root == other.Root || Root == other.Third || Root == other.Fifth ||
+				Third == other.Root || Third == other.Third || Third == other.Fifth ||
+				Fifth == other.Root || Fifth == other.Third || Fifth == other.Fifth;
+		// note: does redundant checks (third with third and fifth with fifth after already checking root with root, and probably others). doubt this will be performance critical, so it stays for now
+	}
+
 }
 
 }
